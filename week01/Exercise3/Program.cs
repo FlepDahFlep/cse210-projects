f9e842dc0a132935e9d57ae4d595cc83a9303f97
flepdahflep <jfernandez4@byupathway.edu>
2026-03-06 19:52:51 +0800

    Finished Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int Number = randomGenerator.Next(1,20);
+        int guess = -1;
+
+        Console.WriteLine("Hey! Welcome to my Guess my Number!");
+        Console.WriteLine("Guess the number between 1 to 20");
+
+        while (guess != Number)
+        {
+            Console.Write("Enter your guess: ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < Number)
+            {
+                Console.WriteLine("Go Higher!");
+            }
+            else if (guess > Number)
+            {
+                Console.WriteLine("Go Lower!");
+            }
+            else
+            {
+                Console.WriteLine("Wow! You guessed it right! Congrats!");
+            }
+        }
     }
 }
