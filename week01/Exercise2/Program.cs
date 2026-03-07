25f06bb017fba41e6b2a07b2c4ef533cb6906664
flepdahflep <jfernandez4@byupathway.edu>
2026-03-07 19:03:40 +0800

    Finished Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,14 +1,16 @@
 using System;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     { 
-        Console.Write("Enter your grade: ");
+        Console.Write("Please enter your grade: ");
         int grade = int.Parse(Console.ReadLine());
 
-        string letter;
+        string letter = "";
+        string sign = "";
 
         if (grade >= 90)
         {
@@ -31,6 +33,35 @@ class Program
             letter = "F";
         }
 
-        Console.WriteLine("Your grade is: " + letter);
+        int lastDigit = grade % 10;
+
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else if (lastDigit < 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        if (letter == "F")
+        {
+            sign = ""; 
+        }
+
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Hey Congrats! You passed the class! We should celebrate!");
+        }
+        else
+        {
+            Console.WriteLine("Aww, there's always a next time, Don't give up! Try again and ask for help!");
+        }
     }
 }
